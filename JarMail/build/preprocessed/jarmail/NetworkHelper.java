05be9b6f3dcd223293b8b28771deb08335eddf3b
Barın Güzeldemirci <baringuzeldemir@gmail.com>
2026-05-30 16:36:08 +0300

    Delete JarMail directory

--- JarMail/build/preprocessed/jarmail/NetworkHelper.java
+++
@@ -1,9 +0,0 @@
-package jarmail;
-
-public class NetworkHelper {
-    public static final String BB_SUFFIX = ";deviceside=true";
-
-    public static String makeBBUrl(String url) {
-        return url + BB_SUFFIX;
-    }
-}
